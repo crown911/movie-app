c5e6f68d5bb2ea91db7933fbda4b2ac951866468
Zabelin Alexander <alex174162@gmail.com>
2026-04-04 00:16:53 +0300

    Добавить начальные файлы для приложения о фильмах

---
+++ script.js
@@ -0,0 +1,10 @@
+const likeButton = document.querySelector(".movie-like");
+const dislikeButton = document.querySelector(".movie-dislike");
+
+likeButton.addEventListener("click", function () {
+  console.log("Like!");
+});
+
+dislikeButton.addEventListener("click", function () {
+  console.log("Dislike!");
+});
